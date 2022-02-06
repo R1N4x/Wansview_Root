e0fd2d0f015f2a6d9df46d29699e2e5ad174103c
R1N4x <git@rinax.de>
2022-02-06 22:55:12 +0100

    fix

--- ipc_after.sh
+++ ipc_after.sh
@@ -1,7 +1,7 @@
 ﻿sleep 5
-echo "-e "\e[1;41m###########################################\e[1;m"
+echo -e "\e[1;41m###########################################\e[1;m"
 echo "Enabling root access"
-echo "-e "\e[1;41m###########################################\e[1;m"
+echo -e "\e[1;41m###########################################\e[1;m"
 sleep 3
 echo -e "\e[1;41m### Setting new root password! ###\e[1;m"
 echo "root:changeme" | chpasswd
