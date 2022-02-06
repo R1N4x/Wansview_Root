4937a82c5f7c1ce5accc7442f9dc0ea1a8c3f228
R1N4x <git@rinax.de>
2022-02-06 22:30:55 +0100

    cleanup

--- example/ipc_after.sh
+++
@@ -1,8 +0,0 @@
-sleep 5
-echo "-e "\e[1;41m###########################################\e[1;m"
-echo "ipc_after.sh triggered! Will run after 10s!"
-echo "-e "\e[1;41m###########################################\e[1;m"
-sleep 10
-echo -e "\e[1;41m### Setting new root password! ###\e[1;m"
-echo "root:changeme" | chpasswd
-echo -e "\e[1;42mDone, have fun!\e[1;m"
