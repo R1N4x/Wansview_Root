8c6c6abf6cfb7590cec62d614cca97bc88e2e3d2
R1N4x <git@rinax.de>
2022-02-06 22:35:47 +0100

    update

---
+++ ipc_after.sh
@@ -0,0 +1,8 @@
+﻿sleep 5
+echo "-e "\e[1;41m###########################################\e[1;m"
+echo "Enabling root access"
+echo "-e "\e[1;41m###########################################\e[1;m"
+sleep 3
+echo -e "\e[1;41m### Setting new root password! ###\e[1;m"
+echo "root:changeme" | chpasswd
+echo -e "\e[1;42mDone, have fun!\e[1;m"
